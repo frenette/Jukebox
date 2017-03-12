4f6733ef1aede4eafe48d591ae6c40d66575610a
Alexander Frenette <alexanderfrenette@Alexanders-MacBook-Pro.local>
2017-03-12 00:04:28 -0700

    The timer works compleately now. It will notify the view at midnight that the status statusLabel needs to be updated. Now there is no need for there to be an event caused by the GUI to update.

--- src/model/PlayInformation.java
+++ src/model/PlayInformation.java
@@ -14,12 +14,12 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class PlayInformation extends Observable {
-    private LocalDate dateOfLastPlayed;
+    // private LocalDate dateOfLastPlayed;
     private int playCount;
     private int maxPlayCount;
 
     public PlayInformation() {
-	this.dateOfLastPlayed = LocalDate.now();
+	// this.dateOfLastPlayed = LocalDate.now();
 	this.playCount = 0;
 	this.maxPlayCount = 3;
 
@@ -29,85 +29,79 @@ public class PlayInformation extends Observable {
 
     // Checks playcount of specific song/student.
     public boolean hasRemaningPlays() {
-//	if (isSameDay()) {
-//	    return this.playCount < this.maxPlayCount;
-//	} else {
-//	    return true;
-//	}
-	
+	// if (isSameDay()) {
+	// return this.playCount < this.maxPlayCount;
+	// } else {
+	// return true;
+	// }
+
 	return this.playCount < this.maxPlayCount;
     }
 
     // returns number of songs played for song/student
     public int songsPlayed() {
-//	if (!isSameDay()) {
-//	    this.dateOfLastPlayed = LocalDate.now();
-//	    this.playCount = 0;
-//	}
+	// if (!isSameDay()) {
+	// this.dateOfLastPlayed = LocalDate.now();
+	// this.playCount = 0;
+	// }
 
 	return this.playCount;
     }
 
     // Increments the playcount of student/song
     public void incrementPlayCount() {
-//	if (isSameDay()) {
-//	    playCount++;
-//	} else {
-//	    // it is a whole new day, new data and new count
-//	    this.dateOfLastPlayed = LocalDate.now();
-//	    this.playCount = 1;
-//	}
-	
+	// if (isSameDay()) {
+	// playCount++;
+	// } else {
+	// // it is a whole new day, new data and new count
+	// this.dateOfLastPlayed = LocalDate.now();
+	// this.playCount = 1;
+	// }
+
 	playCount++;
-	
-//	System.out.println("incrementPlayCount: " + this.playCount);
-//	System.out.println("countObservers for PlayInformation: " + this.countObservers());
+
+	// System.out.println("incrementPlayCount: " + this.playCount);
+	// System.out.println("countObservers for PlayInformation: " +
+	// this.countObservers());
 	this.setChanged();
 	this.notifyObservers();
     }
 
     // Method to reset the play count if it is a new day.
-//    private boolean isSameDay() {
-//	LocalDate currentTime = LocalDate.now();
-//
-//	if (currentTime.getDayOfMonth() == dateOfLastPlayed.getDayOfMonth()) {
-//	    // it is the same day of the month, but it must also be the same
-//	    // month, and same year
-//	    if (currentTime.getMonth() == dateOfLastPlayed.getMonth()) {
-//		// it is the same month, but it must also be the same year
-//		if (currentTime.getYear() == dateOfLastPlayed.getYear()) {
-//		    // it is still the same day
-//		    return true;
-//		}
-//	    }
-//	}
-//
-//	return false;
-//    }
+    // private boolean isSameDay() {
+    // LocalDate currentTime = LocalDate.now();
+    //
+    // if (currentTime.getDayOfMonth() == dateOfLastPlayed.getDayOfMonth()) {
+    // // it is the same day of the month, but it must also be the same
+    // // month, and same year
+    // if (currentTime.getMonth() == dateOfLastPlayed.getMonth()) {
+    // // it is the same month, but it must also be the same year
+    // if (currentTime.getYear() == dateOfLastPlayed.getYear()) {
+    // // it is still the same day
+    // return true;
+    // }
+    // }
+    // }
+    //
+    // return false;
+    // }
 
     /*
      * TESTING : SHOULD SEND MSG AS SOON AS MIDNIGHT HITS
      */
     public void testing() {
+	System.out.println("LOCAL DATE: " + LocalDate.now());
 
-	/*
-	 * LocalTime midnight = LocalTime.MIDNIGHT; LocalDate today =
-	 * LocalDate.now(ZoneId.of("Europe/Berlin")); LocalDateTime
-	 * todayMidnight = LocalDateTime.of(today, midnight); LocalDateTime
-	 * tomorrowMidnight = todayMidnight.plusDays(1);
-	 */
-
-	DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	LocalDate currentDate = LocalDate.now();
-	Date date = null;
 	try {
-	    date = dateFormatter.parse(currentDate + " 23:59:59");
+	    DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	    LocalDate currentDate = LocalDate.now();
+	    Date date = dateFormatter.parse(currentDate + " 24:00:00");
+	    Timer timer = new Timer();
+	    timer.schedule(new MyTimerTask(), date);
 	} catch (ParseException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
 
-	Timer timer = new Timer();
 	// LocalTime midnight = LocalTime.MIDNIGHT;
 	// LocalDate today = LocalDate.now();
 	// LocalDateTime todayMidnight = LocalDateTime.of(today, midnight);
@@ -118,10 +112,8 @@ public class PlayInformation extends Observable {
 	// LocalDateTime ldt = LocalDateTime.of(today, midnight);
 	// Date out = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
 
-	timer.schedule(new MyTimerTask(), date);
 	// System.out.println(todayMidnightDate);
 	// System.out.println(Date.from(Instant.now()).getClass());
-
 
 	// timer.schedule(new MyTimerTask(), Date.from(Instant.now()),
 
@@ -139,8 +131,19 @@ public class PlayInformation extends Observable {
 
 	    // TODO Auto-generated method stub
 	    System.out.println("MyTimerTask has run");
+	    System.out.println("LOCAL DATE: " + LocalDate.now());
 
-	    dateOfLastPlayed = LocalDate.now();
+	    try {
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		LocalDate currentDate = LocalDate.now();
+		Date date = dateFormatter.parse(currentDate + " 24:00:00");
+		Timer timer = new Timer();
+		timer.schedule(new MyTimerTask(), date);
+	    } catch (ParseException e) {
+		e.printStackTrace();
+	    }
+
+	    // dateOfLastPlayed = LocalDate.now();
 	    playCount = 0;
 
 	    setChanged();
